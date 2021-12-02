c581a9180d6ccb5e122a6210c165882361613f12
Webb Barker <robert.barker@coxautoinc.com>
2021-12-01 21:13:56 -0500

    feat(racket): day one part one solution

---
+++ racket/day_1/part-one.rkt
@@ -0,0 +1,28 @@
+#lang racket
+
+(define test-data '("199" "200" "208" "210" "200" "207" "240" "269" "260" "263"))
+(define test-values (map string->number test-data))
+
+(define (windows acc lst n)
+  (if (> n (length lst))
+      acc
+      (windows (cons (take lst n) acc)
+               (rest lst)
+               n)))
+
+(define (solve data)
+  (let ([pairs (windows '() data 2)])
+    (count (lambda (p)
+             (match-let ([(list a b) p])
+               (> b a))) pairs)))
+
+(displayln (format "Test 7 = ~s" (solve test-values)))
+
+(define live-data
+  (call-with-input-file "input.txt"
+    (lambda (in)
+      (sequence->list (in-lines in)))))
+
+(define live-values (map string->number live-data))
+
+(displayln (format "PartOne = ~s" (solve live-values)))
