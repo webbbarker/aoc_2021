5de3df7e3a108d971c274f267c8771addcffa88d
Webb Barker <robert.barker@coxautoinc.com>
2021-12-05 21:38:08 -0500

    feat(day5-p2): day five part two solution

--- rust/challenges/day_5/src/part_two.rs
+++ rust/challenges/day_5/src/part_two.rs
@@ -1,3 +1,98 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let test_data = vec![
+        "0,9 -> 5,9",
+        "8,0 -> 0,8",
+        "9,4 -> 3,4",
+        "2,2 -> 2,1",
+        "7,0 -> 7,4",
+        "6,4 -> 2,0",
+        "0,9 -> 2,9",
+        "3,4 -> 1,4",
+        "0,0 -> 8,8",
+        "5,5 -> 8,2",
+    ];
+
+    let test_values: Vec<((u64, u64), (u64, u64))> = test_data
+        .iter()
+        .map(|&s| {
+            let parts: Vec<&str> = s.split(" -> ").collect();
+            let points: Vec<u64> = parts
+                .iter()
+                .flat_map(|&s| {
+                    s.split(",")
+                        .map(|v| v.parse::<u64>().unwrap())
+                        .collect::<Vec<u64>>()
+                })
+                .collect();
+
+            ((points[0], points[1]), (points[2], points[3]))
+        })
+        .collect();
+
+    println!("[Test] 12 = {}", solve(&test_values));
+
+    if let Ok(live_data) = read_lines("challenges/day_5/input/input.txt") {
+        let raw_data: Vec<String> = live_data.map(|line| line.unwrap()).collect();
+        let live_values: Vec<((u64, u64), (u64, u64))> = raw_data
+            .iter()
+            .map(|s| {
+                let parts: Vec<&str> = s.split(" -> ").collect();
+                let points: Vec<u64> = parts
+                    .iter()
+                    .flat_map(|&s| {
+                        s.split(",")
+                            .map(|v| v.parse::<u64>().unwrap())
+                            .collect::<Vec<u64>>()
+                    })
+                    .collect();
+
+                ((points[0], points[1]), (points[2], points[3]))
+            })
+            .collect();
+
+        println!("[PartOne] {}", solve(&live_values));
+    }
+}
+
+fn solve(data: &[((u64, u64), (u64, u64))]) -> u64 {
+    let mut covered_points: HashMap<(u64, u64), i32> = HashMap::new();
+    for range in data {
+        let p1 = range.0;
+        let p2 = range.1;
+
+        let mut pos = p1;
+        while pos != p2 {
+            *covered_points.entry(pos).or_insert(0) += 1;
+            let (x1, y1) = pos;
+            let (x2, y2) = p2;
+
+            if x1 < x2 {
+                pos.0 += 1;
+            } else if x1 > x2 {
+                pos.0 -= 1;
+            }
+
+            if y1 < y2 {
+                pos.1 += 1;
+            } else if y1 > y2 {
+                pos.1 -= 1;
+            }
+        }
+        *covered_points.entry(p2).or_insert(0) += 1;
+    }
+
+    covered_points.values().filter(|&&v| v >= 2).count() as u64
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
